b8bcecfe618113260cb6acd78920dfd2ac5a998d
Ethemkizilyer <ethemkizilyer3546@gmail.com>
2024-07-13 00:26:17 +0300

    user uploaded photos added

--- controllers/photoController.js
+++ controllers/photoController.js
@@ -1,12 +1,28 @@
-import Photo from '../models/photoModel.js';
+import Photo from "../models/photoModel.js";
 
 const createPhoto = async (req, res) => {
   try {
-    console.log('REQ BODY', req.body);
-    const photo = await Photo.create(req.body);
-    res.status(201).json({
-      succeded: true,
-      photo,
+    console.log("REQ BODY", req.body);
+    await Photo.create({
+      name: req.body.name,
+      description: req.body.description,
+      user: res.locals.user._id,
+    });
+    res.status(201).redirect("/users/dashboard");
+  } catch (error) {
+    res.status(500).json({
+      succeded: false,
+      message: error.message,
+    });
+  }
+};
+
+const getAllPhotos = async (req, res) => {
+  try {
+    const photos = await Photo.find({});
+    res.status(200).render("photos", {
+      photos,
+      link: "photos",
     });
   } catch (error) {
     res.status(500).json({
@@ -16,34 +32,19 @@ const createPhoto = async (req, res) => {
   }
 };
 
-const getAllPhotos = async(req,res)=>{
-    try {
-        const photos= await Photo.find({})
-        res.status(200).render("photos",{
-            photos,
-            link:"photos"
-        })
-    } catch (error) {
-        res.status(500).json({
-            succeded: false,
-            message: error.message,
-          });
-    }
-}
+const getAPhoto = async (req, res) => {
+  try {
+    const photo = await Photo.findById({ _id: req.params.id });
+    res.status(200).render("photo", {
+      photo,
+      link: "photos",
+    });
+  } catch (error) {
+    res.status(500).json({
+      succeded: false,
+      message: error.message,
+    });
+  }
+};
 
-const getAPhoto = async(req,res)=>{
-    try {
-        const photo= await Photo.findById({_id : req.params.id})
-        res.status(200).render("photo",{
-            photo,
-            link:"photos"
-        })
-    } catch (error) {
-        res.status(500).json({
-            succeded: false,
-            message: error.message,
-          });
-    }
-}
-
-export { createPhoto,getAllPhotos,getAPhoto };
+export { createPhoto, getAllPhotos, getAPhoto };
--- models/photoModel.js
+++ models/photoModel.js
@@ -1,4 +1,4 @@
-import mongoose from 'mongoose';
+import mongoose, { Schema } from 'mongoose';
 
 const photoSchema = new mongoose.Schema({
   name: {
@@ -14,6 +14,10 @@ const photoSchema = new mongoose.Schema({
   uploadedAt: {
     type: Date,
     default: Date.now,
+  },
+  user: {
+    type: Schema.Types.ObjectId,
+    ref:"User"
   }
 });
 
